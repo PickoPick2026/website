222a36869272ae7fb10125dc7f386eace3dc6681
Mugesh Rao <mugeshraoego@gmail.com>
2026-09-02 16:10:18 +0530

    3000

--- server.ts
+++ server.ts
@@ -106,7 +106,7 @@ async function sendNriConsultationConfirmation(email: string, name: string, requ
 
 async function startServer() {
   const app = express();
-  const PORT = 3006;
+  const PORT = 3000;
 
  
   app.use(express.json({ limit: "50mb" }));
